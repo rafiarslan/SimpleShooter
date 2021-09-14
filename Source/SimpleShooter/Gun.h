efe36d78e284fea783b3898af1ab883c33da9d0f
Rafi Arslan <raffiarslan@gmail.com>
2021-09-14 12:52:46 +0300

    add gun script and blueprint

---
+++ Source/SimpleShooter/Gun.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Gun.generated.h"
+
+UCLASS()
+class SIMPLESHOOTER_API AGun : public AActor
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent *Root;
+	UPROPERTY(VisibleAnywhere)
+	USkeletalMeshComponent *Mesh;
+
+public:
+	// Sets default values for this actor's properties
+	AGun();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+};
