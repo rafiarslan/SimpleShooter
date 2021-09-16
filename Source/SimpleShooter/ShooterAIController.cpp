56598d0e71626afc928a1c2b51a491fdf3f4d063
Rafi Arslan <raffiarslan@gmail.com>
2021-09-16 11:48:09 +0300

    add new key and setup AI movement sequence

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -13,6 +13,7 @@ void AShooterAIController::BeginPlay()
         RunBehaviorTree(AIBehavior);
         APawn *PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
         GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+        GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
     }
 }
 
