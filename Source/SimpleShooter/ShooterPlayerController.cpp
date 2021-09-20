71e17ed0b8eefd7b19c7d1d067bc1c5d4a436fb1
Rafi Arslan <raffiarslan@gmail.com>
2021-09-20 15:08:58 +0300

    add crosshair and remove some white spaces

--- Source/SimpleShooter/ShooterPlayerController.cpp
+++ Source/SimpleShooter/ShooterPlayerController.cpp
@@ -4,22 +4,32 @@
 #include "TimerManager.h"
 #include "Blueprint/UserWidget.h"
 
-void AShooterPlayerController::GameHasEnded(class AActor* EndGameFocus, bool bIsWinner) 
+void AShooterPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    HUD = CreateWidget(this, HUDClass);
+    if (HUD != nullptr)
+        HUD->AddToViewport();
+}
+
+void AShooterPlayerController::GameHasEnded(class AActor *EndGameFocus, bool bIsWinner)
 {
     Super::GameHasEnded(EndGameFocus, bIsWinner);
 
-     if (bIsWinner)
-     {
+    HUD->RemoveFromViewport();
+    if (bIsWinner)
+    {
         UUserWidget *WinScreen = CreateWidget(this, WinScreenClass);
         if (WinScreen != nullptr)
             WinScreen->AddToViewport();
-     }
-     else
-     {
+    }
+    else
+    {
         UUserWidget *LoseScreen = CreateWidget(this, LoseScreenClass);
         if (LoseScreen != nullptr)
             LoseScreen->AddToViewport();
-     }
+    }
 
     GetWorldTimerManager().SetTimer(RestartTimer, this, &APlayerController::RestartLevel, RestartDelay);
 }
--- Source/SimpleShooter/ShooterPlayerController.h
+++ Source/SimpleShooter/ShooterPlayerController.h
@@ -15,13 +15,20 @@ private:
 	UPROPERTY(EditAnywhere)
 	float RestartDelay = 5.0f;
 	UPROPERTY(EditAnywhere)
+	TSubclassOf<class UUserWidget> HUDClass;
+	UPROPERTY(EditAnywhere)
 	TSubclassOf<class UUserWidget> LoseScreenClass;
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<class UUserWidget> WinScreenClass;
-
+	UPROPERTY(EditAnywhere)
+	UUserWidget *HUD;
+	
 	FTimerHandle RestartTimer;
 
 public:
-	
-	virtual void GameHasEnded(class AActor* EndGameFocus = nullptr, bool bIsWinner = false) override;
+	virtual void GameHasEnded(class AActor *EndGameFocus = nullptr, bool bIsWinner = false) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 };
